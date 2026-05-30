1aceb00ba7d474dc3bb66b048222b43b5625d8d5
Mmmmmagnus <magnustellefsen@gmail.com>
2026-05-30 09:59:57 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/marugoto-app/",
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
 });
